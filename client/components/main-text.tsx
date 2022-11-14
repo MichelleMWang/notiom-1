864a43c50008dcee58b650c8c7a63750801e4579
Michelle Wang <tianw0420@gmail.com>
2022-11-14 11:40:23 -0800

    changed headings

--- client/components/main-text.tsx
+++ client/components/main-text.tsx
@@ -1,12 +1,14 @@
 import { Heading, Box, Center } from "@chakra-ui/react";
 const MainText = () => {
     return (
+        <Center>
         <Box>
             <Center>
                 <Heading size='primary' variant="mainText"> Create. Explore. </Heading>
-                <Heading size='secondary' > The document editing software you’ve been waiting for! </Heading>
             </Center>
+                <Heading size='secondary' > The document editing software you’ve been waiting for! </Heading>        
         </Box>
+        </Center>
     );
 }
 export default MainText; 
